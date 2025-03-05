bc1c6660f3471fca93e5d3d15d7327405e46f6c7
AntonRm <hnso@DESKTOP-0FO4MBM>
2025-03-05 17:33:52 +0200

    added images to lessons quiz

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,19 +82,19 @@
 #   end
 # end
 
-file_path = Rails.root.join('my_temp_files', 'lib', 'story_2.json')
-json_data = JSON.parse(File.read(file_path))
+# file_path = Rails.root.join('my_temp_files', 'lib', 'story_2.json')
+# json_data = JSON.parse(File.read(file_path))
 
-story = Story.create!(name: json_data["name"], visibility: json_data["visibility"])
+# story = Story.create!(name: json_data["name"], visibility: json_data["visibility"])
 
-json_data["questions"].each do |question|
-  story.story_questions.create!(
-    question: question["question"],
-    answer: question["answer"],
-    options: question["options"],
-    coordinates: question["coordinates"]
-  )
-end
+# json_data["questions"].each do |question|
+#   story.story_questions.create!(
+#     question: question["question"],
+#     answer: question["answer"],
+#     options: question["options"],
+#     coordinates: question["coordinates"]
+#   )
+# end
 
 lesson = Lesson.create!(name: "Test Lesson")
 
